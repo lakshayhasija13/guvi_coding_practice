139cb8a58de33a0abac60c254acf6c5d7a4bd02a
Lakshay Hasija <LAKSHAYHASIJA5991@GMAIL.COM>
2026-03-26 17:17:27 +0530

    Implement rounding methods in code.py
    Add two methods to round a number to the nearest greater integer.

---
+++ mathematics/8/code.py
@@ -0,0 +1,17 @@
+N = float(input())
+if N > int(N):
+    print(int(N + 1))
+else:
+    print(int(N))
+
+# Method 2
+"""
+import math
+
+# Input: 2.6
+num = float(input())
+
+# Rounding up to the nearest greater integer
+result = math.ceil(num)
+
+print(result)"""
